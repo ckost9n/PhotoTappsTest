590238ce69f669e3d7621ba38624098bb456f93a
ckost9n <ckost9n@gmail.com>
2021-09-12 14:22:09 +0300

    setup collection  view cell

--- PhotoTappsTest/PhotoCell.swift
+++ PhotoTappsTest/PhotoCell.swift
@@ -9,4 +9,6 @@ import UIKit
 
 class PhotoCell: UICollectionViewCell {
     
+    @IBOutlet var dogImageVIew: UIImageView!
+    
 }
--- PhotoTappsTest/PhotosCollectionViewController.swift
+++ PhotoTappsTest/PhotosCollectionViewController.swift
@@ -7,13 +7,21 @@
 
 import UIKit
 
-let constantNumberSize: CGFloat = 20
-
 class PhotosCollectionViewController: UICollectionViewController {
+    
+    let photos = ["dog1", "dog2", "dog3", "dog4", "dog5", "dog6", "dog7", "dog8", "dog9", "dog10", "dog11", "dog12", "dog13", "dog14", "dog15"]
+    
+    let itemsPerRow: CGFloat = 2
+    let sectionInsert = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+//        let layout = self.collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+//        layout.itemSize = CGSize(width: 70, height: 30)
+//        layout.sectionInset = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+//        layout.minimumLineSpacing = 1
+//        layout.minimumInteritemSpacing = 1
         
     }
 
@@ -27,13 +35,17 @@ class PhotosCollectionViewController: UICollectionViewController {
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 15
+        return photos.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath)
-    
-        cell.backgroundColor = .black
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as! PhotoCell
+        
+        let imageName = photos[indexPath.row]
+        let image = UIImage(named: imageName)
+        
+        cell.dogImageVIew.image = image
+        
     
         return cell
     }
@@ -41,27 +53,25 @@ class PhotosCollectionViewController: UICollectionViewController {
 }
 
 extension PhotosCollectionViewController: UICollectionViewDelegateFlowLayout {
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let itemsPerRow: CGFloat = 2
-        let paddinWidth = constantNumberSize * (itemsPerRow + 1)
+        
+        let paddinWidth = sectionInsert.left * (itemsPerRow + 1)
         let availabelWidth = collectionView.frame.width - paddinWidth
         let widthPerItem = availabelWidth / itemsPerRow
         return CGSize(width: widthPerItem, height: widthPerItem)
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: constantNumberSize, left: constantNumberSize, bottom: constantNumberSize, right: constantNumberSize)
+        return sectionInsert
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return constantNumberSize
+        return sectionInsert.left
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return constantNumberSize
+        return sectionInsert.left
     }
-    
-    
-    
+
 }
