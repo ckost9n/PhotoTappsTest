15a556b27dae1c1ab28caefac4065c51b5144ba4
ckost9n <ckost9n@gmail.com>
2021-09-12 14:53:26 +0300

    add share photos action

--- PhotoTappsTest/PhotoViewController.swift
+++ PhotoTappsTest/PhotoViewController.swift
@@ -20,16 +20,21 @@ class PhotoViewController: UIViewController {
     }
     
     @IBAction func shareAction(_ sender: UIButton) {
+        
+        let shareController = UIActivityViewController(activityItems: [image!], applicationActivities: nil)
+        
+        shareController.completionWithItemsHandler = { _, bool, _, _ in
+            if bool {
+                print("Успешно")
+            } else {
+                print("Не успешно!")
+            }
+        }
+        
+        present(shareController, animated: true, completion: nil)
+        
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
